6231bd617b199f9c14142cf219f27489668aa86b
Ya095 <egor.yakovlev.95@gmail.com>
2023-06-20 10:11:44 +0300

    del some imports

--- src/database.py
+++ src/database.py
@@ -1,7 +1,4 @@
 from typing import AsyncGenerator
-
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
